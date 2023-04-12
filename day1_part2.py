493a271477b34594062f542ca80b429ae9e32dab
Hardi <hardi.kakalaw@chasacademy.se>
2023-04-12 15:53:51 +0200

    testing if if fails
    AssertionError: 51316 != 10

---
+++ day1_part2.py
@@ -0,0 +1,13 @@
+f = open("day1_data.txt")
+masses = f.readlines()
+f.close()
+
+def calculate_fuel(masses):
+    total_fuel = 0
+    for mass in masses:
+        fuel_for_module = 0
+        while mass > 0:
+            mass = mass // 3 - 2 
+            fuel_for_module += max(mass, 0)
+        total_fuel += fuel_for_module
+    return total_fuel
--- test_day1_part2.py
+++ test_day1_part2.py
@@ -1,17 +1,16 @@
 import unittest
+from day1_part2 import calculate_fuel
 
 
 class TestCalculateFuel(unittest.TestCase):
     def test_calculate_fuel(self):
         masses = [12, 14, 1969, 100756]
-        expected_fuel = 5
+        expected_fuel = 10
         self.assertEqual(calculate_fuel(masses), expected_fuel)
 
         self.assertEqual(calculate_fuel([14]), 2)
         self.assertEqual(calculate_fuel([1969]), 966)
         self.assertEqual(calculate_fuel([100756]), 50346)
-
-
 
 
 if __name__ == '__main__':
