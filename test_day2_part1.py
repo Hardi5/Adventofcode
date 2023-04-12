edc266b15a5d77ab817111edd22eac94d342fa4c
Hardi <hardi.kakalaw@chasacademy.se>
2023-04-12 17:21:35 +0200

    pass

--- test_day2_part1.py
+++ test_day2_part1.py
@@ -7,3 +7,4 @@ def test_run_intcode_program():
     assert run_intcode_program([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
     assert run_intcode_program([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
 
+#passing
