38e2f42c350e6c3b175ef31d9975de28b5ececaa
Hardi <hardi.kakalaw@chasacademy.se>
2023-04-12 15:56:46 +0200

    works
    gives: 4856390

--- day1_part2.py
+++ day1_part2.py
@@ -1,5 +1,5 @@
 f = open("day1_data.txt")
-masses = f.readlines()
+masses = [int(line.strip()) for line in f.readlines()]
 f.close()
 
 def calculate_fuel(masses):
@@ -11,3 +11,5 @@ def calculate_fuel(masses):
             fuel_for_module += max(mass, 0)
         total_fuel += fuel_for_module
     return total_fuel
+
+print(calculate_fuel(masses))
